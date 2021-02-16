9cf78bcb8b3af921fcb5eae79006be5d7a83cc0d
Dwire <gjdwyer8@gmail.com>
2021-02-16 10:45:57 -0500

    Revert "test"
    This reverts commit 0b0750ae5e1ad27f27a94c86e150e2d90930be32.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let addToy = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addToy = !addToy;
+    if (addToy) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
