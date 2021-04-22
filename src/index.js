264521e8a5204ce569d78bc03ed55a0f9372aa72
Hpollard003 <hpollard003@gmail.com>
2021-04-22 19:12:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,25 +12,34 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-  toyFetcher()
 });
+//Rendering the API data to webpage
 
-function toyFetcher(){
  fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
   .then(data => cardsRender(data))
   .catch(error => console.error(error))
-  console.log(data)
-}
 
 function cardsRender(toys){
+  toys.forEach(toy => {
+    cardRender(toy)
+  })
+}
+
+function cardRender(toy){
   const card = document.createElement("a")
   const collection = document.getElementById('toy-collection')
-  card.className = 'cards'
-  
+  card.className = 'card'
   card.innerHTML = `
-  <h2> ${toys.name} </h2>
-  <img src="${toys.image}" />
+  <h2> ${toy.name} </h2>
+  <img src="${toy.image}" class="toy-avatar"/>
+  <p> Likes = ${toy.likes} </p>
   `
+const button = document.createElement("button")
+button.innerText = "Like"
 collection.appendChild(card)
+card.appendChild(button)
+return button
 }
+//document.addEventListener('click' , )
+console.log(button)
