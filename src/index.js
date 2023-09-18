e19f2e0f1313ec39b5d4937c4167a0f76a55725d
Kevin <staffordkevinjames@gmail.com>
2023-09-18 13:47:45 -0400

    add toy cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const toysUrl = "http://localhost:3000/toys"
+const divNotOnToysPage = document.querySelector('#toy-collection')
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +16,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// function upDateLikesCounter() {
+//   fetch(toysUrl, {
+//     method: "POST",
+//     headers: 
+//     {
+//       "Content-Type": "application/json", 
+//       Accept: "application/json"
+//     },
+//     body: JSON.stringify({
+//       "likes": newNumberOfLikes
+//     })
+//  })
+//  }
+
+function renderToy(toys) {
+  toys.forEach((toy) => renderToyCard(toy))
+}
+
+function renderToyCard(toy) {
+  const newCard = document.createElement('div')
+  newCard.className = 'card'   
+
+  const h2 = document.createElement('h2')
+  h2.textContent = toy.name
+
+  const img = document.createElement('img')
+  img.className = 'toy-avatar'
+  img.src = toy.image
+
+  const p = document.createElement('p')
+  p.textContent = toy.likes
+
+  const button = document.createElement('button')
+  button.className = 'like-btn'
+  button.id = toy.id
+  button.innerText = 'Like'
+  
+  newCard.append(h2, img, p, button)
+  divNotOnToysPage.append(newCard)
+}
+
+
+function fetchToys() {
+  fetch(toysUrl)
+  .then(Response => Response.json())
+  .then(data => renderToy(data))
+
+}
+
+fetchToys()
+
+document.addEventListener('click', (e) => {
+  const likeBtn = document.getElementsByClassName('like-btn')
+  const likesContainer = document.querySelector('div.card > p')
+  const currentToyId = t
+  const likeNumber = 
+  if()
+  let newNumberOfLikes = likesContainer.textContent++
+   function upDateLikesCounter() {
+    fetch(toysUrl, {
+      method: "POST",
+      headers: 
+      {
+        "Content-Type": "application/json", 
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "likes": newNumberOfLikes
+      })
+   })
+   }
+})
