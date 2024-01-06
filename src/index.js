3339c45002fbed22c100e3434d0091f7a138ebc1
James Hawk <jph94880@gmail.com>
2024-01-05 18:59:39 -0600

    Adds event listener to toy submission form

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    console.log("Clicking Button!")
-    // hide & seek with the form
-    addToy = !addToy;
-    console.log(addToy)
+    addToy = !addToy
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
@@ -27,7 +24,6 @@ function getToys() {
 
 
 function appendToyCard(toys) {
-  console.log("inside appendToys", toys)
   const toysArray = toys.forEach((toy) => {
     const toyCollectionDiv = document.getElementById('#toy-collection')
     const toyCardDiv = document.createElement('div')
@@ -35,13 +31,18 @@ function appendToyCard(toys) {
     const toyImage = document.createElement('img')
     const toyCardLikesCount = document.createElement('p')
     const toyLikeButton = document.createElement('button')
+    const toyId = toy.id
     toyCardDiv.setAttribute('class', 'card')
-    toyNameH2.innerText = toy.name
+    toyNameH2.textContent = toy.name
     toyImage.setAttribute("src", toy.image)
     toyImage.setAttribute("class", "toy-avatar")
     toyLikeButton.setAttribute("class", "like-btn")
     toyLikeButton.setAttribute("id", toy.id)
     toyLikeButton.innerText = "Like ❤️"
+    toyCardLikesCount.innerText = `Likes: ${toy.likes}`
+
+
+
     toyCardDiv.append(toyNameH2)
     toyCardDiv.append(toyImage)
     toyCardDiv.append(toyCardLikesCount)
@@ -50,6 +51,25 @@ function appendToyCard(toys) {
   })
 }
 
+const formToAddToyHTMLCollection = document.getElementsByClassName('add-toy-form')
+const formToAddToyArray = Array.from(formToAddToyHTMLCollection)  
+const formToAddToyDiv = formToAddToyArray[0]
+console.log(formToAddToyDiv)
+
+const addNewToy = () => {
+
+
+}
+
+formToAddToyDiv.addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log(e)
+  console.log(e.target[0].value)
+  console.log(e.target[1].value)
+  console.log(e.target[2].value)
+})
+
+addNewToy()
 
 /* 
 
